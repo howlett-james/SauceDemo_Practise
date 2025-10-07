17d4794a35c3bccf2dfa4682dc0dab36d487e525
johnsenthil <sajohn1999@gmail.com>
2025-10-07 21:41:16 +0530

    Product changes

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,8 +1,6 @@
 package pages;
 
 import base.DriverFactory;
-import org.apache.hc.core5.util.Asserts;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.util.Objects;
@@ -21,7 +19,4 @@ public class HomePage {
     public String getTitle() {
         return driver.getTitle();
     }
-
-
-    }
-
+}
